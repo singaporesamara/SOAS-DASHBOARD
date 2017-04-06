2d90d378dee1343c83cb17bef4da9c1ea38634f0
anton.shabanov <anton.shabanov@castle.co>
2017-04-06 22:28:37 +0400

    ForgotPassword sagas.

--- app/containers/User/ForgotPasswordPage/sagas.js
+++ app/containers/User/ForgotPasswordPage/sagas.js
@@ -1,13 +1,19 @@
-import { put, takeLatest } from 'redux-saga/effects';
+import { put, takeLatest, call } from 'redux-saga/effects';
 import { requestStarted, requestFinished } from '../../../actions/common';
 import { RESTORE_PASSWORD } from './constants';
 import { restorationResent } from './actions';
+import routes from '../../../utils/network/api';
 
 function* restorePasswordSaga({ payload: { email } }) {
   yield put(requestStarted());
-  console.info(email);
+  const response = yield call(routes.user.forgotPassword, { email });
   yield put(requestFinished());
-  yield put(restorationResent());
+
+  if (response.err) {
+    alert('error...');
+  } else {
+    yield put(restorationResent());
+  }
 }
 
 export function* forgotPasswordFlow() {
