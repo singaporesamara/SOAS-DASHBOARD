6edb749cb7db97df2171a6bd5d64fe6710648fcd
anton.shabanov <anton.shabanov@castle.co>
2017-04-10 04:28:02 +0400

    Home page redirect.

--- app/containers/HomePage/reducer.js
+++ app/containers/HomePage/reducer.js
@@ -1,35 +1,14 @@
-/*
- * HomeReducer
- *
- * The reducer takes care of our data. Using actions, we can change our
- * application state.
- * To add a new action, add it to the switch statement in the reducer function
- *
- * Example:
- * case YOUR_ACTION_CONSTANT:
- *   return state.set('yourStateVariable', true);
- */
 import { fromJS } from 'immutable';
+import { merge } from 'lodash';
+import { DEFAULT_PAGE_STATE } from '../../constants/common';
 
-import {
-  CHANGE_USERNAME,
-} from './constants';
+const initialState = fromJS(merge({}, DEFAULT_PAGE_STATE));
 
-// The initial state of the App
-const initialState = fromJS({
-  username: '',
-});
-
-function homeReducer(state = initialState, action) {
+function pageReducer(state = initialState, action) {
   switch (action.type) {
-    case CHANGE_USERNAME:
-
-      // Delete prefixed '@' from the github username
-      return state
-        .set('username', action.name.replace(/@/gi, ''));
     default:
       return state;
   }
 }
 
-export default homeReducer;
+export default pageReducer;
--- app/sagas/index.js
+++ app/sagas/index.js
@@ -4,12 +4,13 @@ import forgotPasswordSaga from '../containers/User/ForgotPasswordPage/sagas';
 import changePasswordSaga from '../containers/User/ChangePasswordPage/sagas';
 import registrationSaga from '../containers/User/RegistrationPage/sagas';
 import applicationHomeSaga from '../containers/Application/HomePage/sagas';
+import homeSaga from '../containers/HomePage/sagas';
 
 import formSaga from './common/form';
 import userSaga from './common/user';
 
 const addSagas = (store) => {
-  const allSagas = [formSaga, userSaga, loginSaga, signUpSaga, forgotPasswordSaga, changePasswordSaga, registrationSaga, applicationHomeSaga];
+  const allSagas = [formSaga, userSaga, loginSaga, signUpSaga, forgotPasswordSaga, changePasswordSaga, registrationSaga, applicationHomeSaga, homeSaga];
 
   allSagas.forEach((sagas) => {
     sagas.forEach((saga) => {
